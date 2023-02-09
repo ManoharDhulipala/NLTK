c9e75e9dee3e886f8e07ece49402de3cd0cf8c60
ManoharDhulipala <m_dhulipala@u.pacific.edu>
2023-02-09 12:41:06 -0800

    Added more examples to test sentimental analysis and restructed code

--- VADER.py
+++ VADER.py
@@ -1,27 +1,36 @@
 import nltk
 from nltk.sentiment import SentimentIntensityAnalyzer
 from nltk.tokenize import TweetTokenizer, sent_tokenize
-
-example = """Professor Jones is very nice. He does a great job at lecturing and his exams are fair. 
-One thing that I wish he would do is pause more often because I can't write my notes thoroughly during lecture."""
-
-tokens = TweetTokenizer()
-tokens_sentences = [tokens.tokenize(t) for t in nltk.sent_tokenize(example)]
-joinedSentences = []
-
-for sentences in tokens_sentences:
-	sentences = " ".join(sentences)
-	joinedSentences.append(sentences)
-
-sia = SentimentIntensityAnalyzer()
 positiveList = []
 negativeList = []
-for sentence in joinedSentences:
-	data = sia.polarity_scores(sentence)
+def sentimentalAnalysis(example):
+	tokens = TweetTokenizer()
+	tokens_sentences = [tokens.tokenize(t) for t in nltk.sent_tokenize(example)]
+	joinedSentences = []
+
+	for sentences in tokens_sentences:
+		sentences = " ".join(sentences)
+		joinedSentences.append(sentences)
+
+	sia = SentimentIntensityAnalyzer()
+	for sentence in joinedSentences:
+		data = sia.polarity_scores(sentence)
+		print(sentence, " Polarity Score: ", data)
 	if data["pos"] > 0.3:
 		positiveList.append(sentence)
 	else:
 		negativeList.append(sentence)
 	
-print(positiveList)
-print(negativeList)
+
+
+lines = []
+
+with open("negativeExamples.txt") as NumFile:
+
+	[lines.append(line.strip("\n")) for line in NumFile.readlines()]
+
+for sentences in lines:
+	sentimentalAnalysis(sentences)
+
+print("Positive Lines: ", positiveList)
+print("Negative Lines: ", negativeList)
