3298c4a7d351c2439b22db5a9cefe68424c93887
ManoharDhulipala <m_dhulipala@u.pacific.edu>
2023-03-10 11:56:18 -0800

    Sentimental analysis working at 75% accuracy with rate my professor data.

--- rmp.py
+++ rmp.py
@@ -15,7 +15,27 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score 
 from textblob import TextBlob
 df = pandas.read_csv("RateMyProfData.csv")
+from nltk.corpus import stopwords
+i = 0
+correct = 0
+generatedReview = []
 for reviews in df.Review:
+	actualReview = df.Rating[i]
 	reviews = TextBlob(reviews)
-	print(reviews)
-	print(reviews.sentiment)
+	tokens=set(reviews.words)
+	# stopwords
+	stop=set(stopwords.words("english"))
+	tokenized = tokens-stop
+	tokenized  = ', '.join(tokenized)
+	# Removing stop words using set difference operation
+	tokenized = TextBlob(tokenized)
+	if (tokenized.sentiment.polarity > 0):
+		generatedReview.append("Positive")
+	else:
+		generatedReview.append("Negative")
+	if (generatedReview[-1] ==  actualReview):
+		correct = correct + 1
+	print("Review: ", generatedReview[-1], " Actual Review: ", actualReview)
+
+	i = i + 1
+print("Accuracy: ", correct/(i+1))
